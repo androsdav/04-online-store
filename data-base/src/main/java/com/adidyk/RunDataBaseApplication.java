0f0d565508fd1127701dade9f98cf6231dd2249c
androsdav <androsdav@gmail.com>
2020-09-09 17:27:41 +0300

    refactoring

--- data-base/src/main/java/com/adidyk/RunDataBaseApplication.java
+++ data-base/src/main/java/com/adidyk/RunDataBaseApplication.java
@@ -16,6 +16,10 @@ public class RunDataBaseApplication {
 
     private UserRepository userRepository;
 
+    /**
+     * RunDataBaseApplication - constructor.
+     * @param userRepository - user repository.
+     */
     @Autowired
     public RunDataBaseApplication(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -29,7 +33,6 @@ public class RunDataBaseApplication {
         SpringApplication.run(RunDataBaseApplication.class, arg);
     }
 
-
     @EventListener(ApplicationReadyEvent.class)
     public void testJpaMethods() {
         User user1 = new User("11111", "11111", "11111", "11111");
@@ -42,85 +45,5 @@ public class RunDataBaseApplication {
         this.userRepository.save(user4);
         System.out.println("Find all user: " + this.userRepository.findAll());
     }
-        //User user = new User();
-        //user
-        //user.setFirstName("first-name");
-        //user.
-
-        // add smart phone
-        //SmartPhone iphone = new SmartPhone("samsung", "galaxy", "smart phone", 10, 500);
-        //this.smartPhoneService.save(iphone);
-
-        // add order
-        //Order order = new Order();
-
-        // add user
-        //User user = this.userService.findById(5);
-        //System.out.println();
-        //System.out.println();
-        //System.out.println(user);
-        //this.userService.save(user);
-        //Order order = new Order(new Date());
-        //ArrayList<SmartPhone> list = new ArrayList<>();
-        //list.add(new SmartPhone(1));
-        //list.add(new SmartPhone(3));
-        //order.setSmartPhones(list);
-        //order.setUser(new User(5));
-        //ArrayList<Order> listOrder = new ArrayList<>();
-        //listOrder.add(order);
-        //user.setOrders(listOrder);
-        //System.out.println();
-        //System.out.println();
-        //System.out.println(user);
-        //this.userService.save(user);
-        //order.setUser(new User(3));
-        //this.orderService.save(order);
-
-
-
-
-        //Order order = new Order("order 1");
-        //this.orderService.save(order);
-        /*
-        Order order = this.orderService.findById(1);
-        ArrayList<SmartPhone> list = new ArrayList<>();
-        list.add(new SmartPhone(3));
-        list.add(new SmartPhone(2));
-        order.setSmartPhones(list);
-        this.orderService.save(order);
-        System.out.println(this.orderService.findById(1).getSmartPhones());
-        */
-        //Order order = new Order("order 2");
-        //this.orderService.save(order);
-
-
-        /*
-        SmartPhone iphone = new SmartPhone("apple", "70", "smart phone", 10);
-        SmartPhone nokia = new SmartPhone("nokia", "71", "smart phone", 5);
-        SmartPhone samsung = new SmartPhone("samsung", "galaxy", "smart phone", 3);
-        SmartPhone lg = new SmartPhone("lg", "11", "smart phone", 4);
-        this.service.add(iphone);
-        this.service.add(nokia);
-        this.service.add(samsung);
-        this.service.add(lg);
-        */
-        /*
-        SmartPhone lg = new SmartPhone(10,"lg", "11", "smart phone", 66);
-        //this.service.update(lg);
-        System.out.println(this.service.get(7));
-        System.out.println(this.service.get(8));
-        System.out.println(this.service.get(9));
-        System.out.println(this.service.get(10));
-        System.out.println(this.service.get(11));
-
-        for (SmartPhone smartPhone : this.service.getAll()) {
-            System.out.println(smartPhone);
-        }
-        SmartPhone item = new SmartPhone();
-        item.setCompany("nokia");
-        System.out.println(this.service.findAllByCompany(item));
-        */
-
-
 
 }
