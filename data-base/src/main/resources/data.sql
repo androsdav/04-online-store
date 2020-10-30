967100a1de62b6e2486273f24081aa0c0b756cff
androsdav <androsdav@gmail.com>
2020-10-30 17:10:48 +0200

    adds data to data base

--- data-base/src/main/resources/data.sql
+++ data-base/src/main/resources/data.sql
@@ -1,4 +1,49 @@
+-------------------------------------------------------------------------------------------------------------------------
+INSERT INTO laptop_general(product_name, brand, model_number, color, color_category) VALUES
+(
+ 'Pavilion x360 2-in-1 14" Touch-Screen Laptop - Intel Core i5 - 8GB Memory - 256GB SSD',
+ 'HP',
+ '14M-DW0023DX',
+ 'Luminous Gold',
+ 'Gold'
+);
+INSERT INTO laptop_display(screen_size, screen_resolution, touch_screen, display_type) VALUES
+(
+ 14,
+ '1920 x 1080',
+ 'Yes',
+ 'Full HD Widescreen LED'
+);
+INSERT INTO laptop_storage(storage_type, total_storage_capacity, solid_state_drive_capacity, solid_state_drive_interface) VALUES
+(
+ 'SSD',
+ '256',
+ '256',
+ 'PCIe'
+);
+INSERT INTO laptop_memory(system_memory_RAM, type_of_memory_RAM, system_memory_RAM_speed) VALUES
+(
+ '8',
+ 'DDR4 SDRAM',
+ '3200'
+);
+INSERT INTO laptop_processor(processor_brand, processor_speed_base, processor_model, processor_model_number, processor_cores) VALUES
+(
+ 'Intel',
+ '1',
+ 'Intel 10th Generation Core i5',
+ 'i5-1035G1',
+ '4'
+);
+INSERT INTO laptop(general_id, display_id, storage_id, memory_id, processor_id) VALUES( '1', '1', '1', '1', '1');
+-------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
 -- adds type transport to table type_transport
+/*
 INSERT INTO type_transport(name) VALUES('car');
 INSERT INTO type_transport (name) VALUES ('motorcycle');
 
